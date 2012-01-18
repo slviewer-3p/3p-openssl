c3802f2be498aae2e880b126645f7c65839e2a21
Logan Dethrow <log@lindenlab.com>
2012-01-18 16:35:32 -0500

    Fixed library rename and set build concurrency back to 1, since it seemed to be causing build failures.

--- build-cmd.sh
+++ build-cmd.sh
@@ -69,19 +69,18 @@ cd "$OPENSSL_SOURCE_DIR"
         "linux")
             # "shared" means build shared and static, instead of just static.
             ./Configure shared no-idea linux-generic32 -fno-stack-protector -m32 --prefix="$stage"
-            make -j2
+            make
 
             make install
             mkdir -p "$stage/lib/release"
-            mv "$stage/lib/*.a" "$stage/release"
-            mv "$stage/lib/*so*" "$stage/release"
+            mv "$stage/lib/"*.a "$stage/lib/release"
+            mv "$stage/lib/"*so* "$stage/lib/release"
              
             # By default, 'make install' leaves even the user write bit off.
             # This causes trouble for us down the road, along about the time
             # the consuming build tries to strip libraries.
             chmod u+w "$stage/lib/release"/libcrypto.so.* "$stage/lib/release"/libssl.so.*
             
-            make clean
         ;;
     esac
     mkdir -p "$stage/LICENSES"
