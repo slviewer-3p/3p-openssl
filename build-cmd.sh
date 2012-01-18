9d7e24e8fb1e66e186df9c4b316c0474f2deca80
Logan Dethrow <log@lindenlab.com>
2012-01-18 16:03:58 -0500

    Added static build of libssl and libcrypto to linux package.

--- build-cmd.sh
+++ build-cmd.sh
@@ -67,18 +67,21 @@ cd "$OPENSSL_SOURCE_DIR"
             make install
         ;;
         "linux")
-			./Configure shared no-idea linux-generic32 -fno-stack-protector -m32 --prefix="$stage"
-            make
+            # "shared" means build shared and static, instead of just static.
+            ./Configure shared no-idea linux-generic32 -fno-stack-protector -m32 --prefix="$stage"
+            make -j2
+
             make install
-
-            mv "$stage/lib" "$stage/release"
-            mkdir -p "$stage/lib"
-            mv "$stage/release" "$stage/lib"
-
+            mkdir -p "$stage/lib/release"
+            mv "$stage/lib/*.a" "$stage/release"
+            mv "$stage/lib/*so*" "$stage/release"
+             
             # By default, 'make install' leaves even the user write bit off.
             # This causes trouble for us down the road, along about the time
             # the consuming build tries to strip libraries.
             chmod u+w "$stage/lib/release"/libcrypto.so.* "$stage/lib/release"/libssl.so.*
+            
+            make clean
         ;;
     esac
     mkdir -p "$stage/LICENSES"
